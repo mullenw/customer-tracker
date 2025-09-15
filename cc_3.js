c7fafa354a66e85f1676d4871aba3593b3fe1bd9
Wyatt Mullen <mullenw@usf.edu>
2025-09-14 22:23:58 -0400

    updated customer info and purchases

---
+++ cc_3.js
@@ -0,0 +1,26 @@
+let customers = [
+    {
+        name: "Amon-Ra St. Brown",
+        email: "astbrown@gmail.com",
+        purchases: ["football cleats", "gloves"]
+    },
+    {
+        name: "James Cook",
+        email: "jcook@gmail.com"
+        purchases: ["helmet", "shoulder pads"]
+    },
+    {
+        name: "De'von Achane"
+        email: "dachane@gmail.com"
+        purchases: ["blue gatorade", "gloves"]
+    }
+];
+
+//Summary of Products
+//SKU-001 | Eco Bottle | $19.99 | Stock: 42
+inventory.forEach(obj => console.log(`${obj.sku}\t|\t${obj.name}\t|\t${obj.price}\t|\t${obj.stock}`));
+
+inventory.push({sku:"SKU-005",name:"Pads",price:174.99,stock:150})
+console.log(inventory.pop())
+inventory[1].price = 19.99
+console.log(inventory[1])
